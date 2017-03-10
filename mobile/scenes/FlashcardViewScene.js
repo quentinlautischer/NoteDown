d928dad0e50ec4408bd430c6f19f10527fa511ce
hailey123 <musselma@ualberta.ca>
2017-03-10 08:01:22 -0700

    landscape for ios

--- mobile/scenes/FlashcardViewScene.js
+++ mobile/scenes/FlashcardViewScene.js
@@ -6,7 +6,26 @@ import {
     TouchableHighlight
 } from 'react-native';
 
+var Orientation = require('react-native-orientation');
+
 export default class FlashcardViewScene extends Component {
+    componentDidMount() {
+
+        Orientation.getOrientation((err,orientation)=> {
+            console.log("Current Device Orientation: ", orientation);
+        });
+
+        // Orientation.lockToPortrait(); //this will lock the view to Portrait
+        Orientation.lockToLandscape(); //this will lock the view to Landscape
+        //Orientation.unlockAllOrientations(); //this will unlock the view to all Orientations
+
+
+    }
+
+    componentWillUnmount() {
+        Orientation.lockToPortrait();
+    }
+
     render() {
         return (
             <View>
