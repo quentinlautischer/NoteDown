c9ac6d4e967eeef81e4eb90972f8bc5ea1f0ee76
hailey123 <musselma@ualberta.ca>
2017-03-25 12:29:17 -0600

    started rev parser, h1s working

---
+++ shared/reverse-parser.js
@@ -0,0 +1,33 @@
+console.log(reverseParse(`
+    <h1>This is heading 1</h1>
+    <h2>This is heading 2</h2>
+    <h3>This is heading 3</h3>
+    <h4>This is heading 4</h4>
+    <h5>This is heading 5</h5>
+    <h6>This is heading 6</h6>
+    <h1>This is another heading 1</h1>
+    <p>This is some text.</p>
+    <h2>This is another heading 2</h2>
+    <p>This is some other text.</p>
+`));
+
+
+function reverseParse(str) {
+    return parseHeaders(str);
+}
+
+function parseHeaders(str) {
+    console.log(str);
+    var pattern = /(<h1>(.*?)<\/h1>)/;
+    var match = pattern.exec(str);
+    while (match != null) {
+        var whole = match[1];
+        var inner = match[2];
+        console.log(match[1]);
+        console.log(match[2]);
+        str = str.replace(whole, "# " + inner);
+        match = pattern.exec(str);
+    }
+
+    return str;
+}
